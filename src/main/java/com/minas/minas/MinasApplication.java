11b89a7982eb29e75a579569c9cd2c70e4565e2c
Serena Ribeiro <serena.ribeiro@ericsson.com>
2019-08-14 15:24:23 +0100

    :tada: Create project structure

---
+++ src/main/java/com/minas/minas/MinasApplication.java
@@ -0,0 +1,13 @@
+package com.minas.minas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinasApplication.class, args);
+	}
+
+}
